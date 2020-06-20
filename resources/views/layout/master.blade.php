c3f2c5ed4d8f56cf4747ac9161a0456df5b737e1
Christian Harvey <hikusabi@gmail.com>
2020-06-20 13:33:38 +0100

    Added logo and favicon and prepared first view for testing

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title', 'Forager')</title>
+        <meta name="description" content="An application to assist a community in discovering local nature trails, plants and wildlife">
+        <meta name="author" content="Christian Harvey">
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+</html>
